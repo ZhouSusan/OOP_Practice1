15bf5af96f43a1cdd9e0663b65194feb3a0ddcdb
ZhouSusan <szhou089@gmail.com>
2022-11-08 18:34:20 -0800

    added class Musician and Musician.java file

---
+++ src/main/java/org/example/Musician.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Musician {
+    Instrument myInstrument;
+    String name;
+
+    public Musician(Instrument instrument, String name){
+        myInstrument = instrument;
+        this.name = name;
+    }
+
+    void play(){
+
+    }
+}
