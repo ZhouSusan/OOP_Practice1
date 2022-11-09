ab23bf0668f741639f5cd62ca000f2cfa9a38191
ZhouSusan <szhou089@gmail.com>
2022-11-08 20:55:03 -0800

    created piano object in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        Piano piano = new Piano("Piano");
+        piano.sound();
+
         Drum drums = new Drum("Drums");
         Drummer emma = new Drummer(drums, "Emma");
-        drums.sound();
         emma.play();
     }
 }
